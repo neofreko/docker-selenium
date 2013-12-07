6c1935530011aff225857856490b958306aaa3ed
Akhmad Fathonih <akhmadf@gmail.com>
2013-12-07 10:21:26 +0900

    add option to choose selenium version

--- install/usr/local/bin/start-selenium-server.sh
+++ install/usr/local/bin/start-selenium-server.sh
@@ -2,4 +2,21 @@
 
 export DISPLAY=:99
 /etc/init.d/xvfb start
-java -jar /srv/selenium-server-standalone-2.35.0.jar
+
+case "$1" in
+  2.31)
+    export VERSION=2.31.0
+    ;;
+  2.35)
+    export VERSION=2.35.0
+    ;;
+  help)
+    "$0" help
+    echo "only support 2.31 and 2.35. default to 2.31"
+    echo ""
+    ;;
+  *)
+    export VERSION=2.31.0
+esac
+
+java -jar /srv/selenium-server-standalone-$VERSION.jar
